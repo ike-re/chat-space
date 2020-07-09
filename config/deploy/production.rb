b533e4a435fc8107e96d9b640cbeff5fa5cdc7b6
ike-re <67263156+ike-re@users.noreply.github.com>
2020-07-09 14:18:31 +0900

    daploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.181.134.196/', user: 'ec2-user', roles: %w{app db web}
+server '18.181.134.196', user: 'ec2-user', roles: %w{app db web}
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
